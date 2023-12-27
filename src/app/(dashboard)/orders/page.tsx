fae47e951ed08cad6a6403ab463fd97deeab447f
dncuong <dinhcuongnd2001@gmail.com>
2023-12-27 08:56:46 +0700

    feat: order screen

--- src/app/(dashboard)/orders/page.tsx
+++ src/app/(dashboard)/orders/page.tsx
@@ -75,7 +75,7 @@ const OrdersPage = () => {
                 {orderData.map((order) => (
                   <tr key={order.id}>
                     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                      {order.name}
+                      {order.id}
                     </td>
                     <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
                       {order.name}
